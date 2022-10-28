f9bc3210e8cb2f7cacea8d4cb80ad5d375dcea4d
sandyy-mutex <nathanhackett71@gmail.com>
2022-10-28 12:56:00 +0100

    Improvements
    Cleaned up code in /index.js Improved UI Added custom API support Needs reviewing ; Added serve.sh

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let giveCatButton = document.getElementById('give-cat')
 
-giveCatButton.addEventListener("click", evt => {
-    let catDiv = document.getElementById('cat-pic')
-
-    fetch("https://api.thecatapi.com/v1/images/search?")
-    .then(res => res.json())
-    .then(cats => {
-        cats.forEach(cat => {
-            catDiv.innerHTML = `<h3>Here is this cat! </h3>
-            <img src="${cat.url}" alt="kitty" />`
-        })
+giveCatButton.addEventListener("click", _ => {
+    let url = "https://api.thecatapi.com/v1/images/search?"
+    if (document.getElementById("custom-api").value) {
+        url = document.getElementById("custom-api").value
+    }
+    fetch(url)
+    .then((res) => res.json())
+    .then((cat) => {
+        let c = document.getElementById("cat")
+        c.src = cat[0].url
+        c.hidden = false
     })
 })
---
+++ serve.sh
@@ -0,0 +1,2 @@
+bash xdg-open "http://127.0.0.1:8080"
+npx http-server
